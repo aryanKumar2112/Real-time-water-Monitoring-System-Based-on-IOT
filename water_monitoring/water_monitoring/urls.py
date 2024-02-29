bf000cd875a87727ad0a032ec7322a8cfbab4d83
AmanJaiswal1810 <aj3889976@gmail.com>
2024-02-29 16:27:11 +0530

    Login added

--- water_monitoring/water_monitoring/urls.py
+++ water_monitoring/water_monitoring/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from iotData.views import iotDataView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/iotData/', iotDataView.as_view()),
+    path('', include('iotData.urls'))
 ]
 
