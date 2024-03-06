b047a8af4e94c006191d56e7ecca45727f1db0ac
AmanJaiswal1810 <aj3889976@gmail.com>
2024-03-06 13:28:37 +0000

    Static Root

--- water_monitoring/water_monitoring/urls.py
+++ water_monitoring/water_monitoring/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from iotData.views import iotDataView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/iotData/', iotDataView.as_view()),
